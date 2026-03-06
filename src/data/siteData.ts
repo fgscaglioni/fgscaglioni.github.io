e53071da3c6349a4c7150f11201cc54b9d8d75f2
Fabrício Scaglioni <fgscaglioni@gmail.com>
2026-03-06 15:10:00 -0300

    feat: implement portfolio page and update global styles to a brutalist aesthetic.

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -8,7 +8,9 @@ export const my_stack = {
             { name: 'PHP', image: 'https://e7.pngegg.com/pngimages/185/655/png-clipart-logo-php-computer-icons-symbol-miscellaneous-emblem-thumbnail.png', url: 'https://www.php.net/' },
             { name: 'JavaScript', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuHnJDLOcdm_0b6N6kNj-1OvO9KhKYgqIy0w&s', url: '' },
             { name: 'TypeScript', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Typescript.svg/1200px-Typescript.svg.png', url: 'https://www.typescriptlang.org/' },
-            { name: 'Python', image: 'https://raw.githubusercontent.com/docker-library/docs/01c12653951b2fe592c1f93a13b4e289ada0e3a1/python/logo.png', url: 'https://www.python.org/' }
+            { name: 'Python', image: 'https://raw.githubusercontent.com/docker-library/docs/01c12653951b2fe592c1f93a13b4e289ada0e3a1/python/logo.png', url: 'https://www.python.org/' },
+            { name: 'HTML', image: 'https://bognarjunior.wordpress.com/wp-content/uploads/2014/12/1417589451_html-256.png', url: '' },
+            { name: 'CSS', image: 'https://ralfvanveen.com/wp-content/uploads/2021/06/CSS-_-Begrippenlijst.svg', url: '' },
         ]
     },
     databases: {
@@ -28,28 +30,25 @@ export const my_stack = {
     },
     frameworks: {
         color: '#282A3E',
-        title: 'Frameworks',
+        title: 'Frameworks & Bibliotecas',
         data: [
             { name: 'Laravel', image: 'https://laravel.com/img/logotype.min.svg', url: 'https://laravel.com/' },
             { name: 'CodeIgniter', image: 'https://packagecontrol.io/readmes/img/dfb5fec47c7a7e44d09fb1631dfe0f41806bce27.png', url: 'https://www.codeigniter.com/' },
             { name: 'Ionic', image: 'https://www.wonderpush.com/wp-content/uploads/2019/09/ionic.png', url: 'https://ionicframework.com/' },
             { name: 'Angular', image: 'https://angular.io/assets/images/logos/angular/angular.svg', url: 'https://angular.dev/' },
             { name: 'NestJS', image: 'https://nestjs.com/img/logo-small.svg', url: 'https://nestjs.com/' },
+            { name: 'React', image: 'https://images.seeklogo.com/logo-png/27/2/react-logo-png_seeklogo-273845.png', url: 'https://react.dev/' },
             { name: 'AdonisJS', image: 'https://avatars.githubusercontent.com/u/13810373?s=280&v=4', url: 'https://adonisjs.com/' },
             { name: 'Svelte', image: 'https://svelte.dev/svelte-logo-horizontal.svg', url: 'https://svelte.dev/' },
-            { name: 'Unsloth', image: 'https://avatars.githubusercontent.com/u/150920049?s=280&v=4', url: 'https://unsloth.ai/' }
+            { name: 'Apache Cordova', image: 'https://cordova.apache.org/static/img/cordova_bot.png', url: 'https://cordova.apache.org/' },
+            { name: 'jQuery', image: 'https://miro.medium.com/v2/resize:fit:860/0*eFomJUFua8tuqe8g.png', url: 'https://jquery.com/' },
+            { name: 'jQuery UI', image: 'https://cdn.simpleicons.org/jquery/0769AD', url: 'https://jqueryui.com/' },
         ]
     },
     others: {
         color: '#FF7033',
         title: 'Outras Tecnologias',
         data: [
-            { name: 'React', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png', url: 'https://react.dev/' },
-            { name: 'HTML', image: 'https://bognarjunior.wordpress.com/wp-content/uploads/2014/12/1417589451_html-256.png', url: '' },
-            { name: 'CSS', image: 'https://ralfvanveen.com/wp-content/uploads/2021/06/CSS-_-Begrippenlijst.svg', url: '' },
-            { name: 'jQuery', image: 'https://miro.medium.com/v2/resize:fit:860/0*eFomJUFua8tuqe8g.png', url: 'https://jquery.com/' },
-            { name: 'jQuery UI', image: 'https://blog.dankicode.com/wp-content/uploads/2019/12/jquery-ui-650x400.png', url: 'https://jqueryui.com/' },
-            { name: 'Apache Cordova', image: 'https://cordova.apache.org/static/img/cordova_bot.png', url: 'https://cordova.apache.org/' },
             { name: 'Android', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDHvnyoyWI-FlgTfOWRoJjazD3ke1Q6hOgfQ&s', url: 'https://www.android.com/intl/pt_br/' },
             { name: 'n8n', image: 'https://registry.npmmirror.com/@lobehub/icons-static-png/latest/files/dark/n8n-color.png', url: 'https://n8n.io/' },
             { name: 'Coolify', image: 'https://docs.hetzner.com/static/1dbc8e5220638f7193ef9f5a24c2eb5b/0b533/coolify-logo.png', url: 'https://coolify.io/' },
@@ -57,15 +56,15 @@ export const my_stack = {
             { name: 'Firebase', image: 'https://firebase.google.com/static/images/brand-guidelines/logo-vertical.png?hl=pt-br', url: 'https://firebase.google.com/' },
             { name: 'Docker', image: 'https://raw.githubusercontent.com/collabnix/dockerlabs/master/beginners/docker/images/docker_facebook_share.png', url: 'https://www.docker.com/' },
             { name: 'Docsify', image: 'https://images.opencollective.com/docsify/5916949/logo/256.png', url: 'https://docsify.js.org/' },
+            { name: 'Unsloth', image: 'https://avatars.githubusercontent.com/u/150920049?s=280&v=4', url: 'https://unsloth.ai/' }
         ]
     },
     methodologies: {
         color: '#02040F',
         title: 'Metodologias',
         data: [
-            { name: 'Scrum', image: 'https://www.scrum.org/sites/default/files/scrum-org-logo.png', url: 'https://www.scrum.org/' },
-            { name: 'Kanban', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Kanban_logo.svg/1200px-Kanban_logo.svg.png', url: 'https://kanbanize.com/kanban-resources/getting-started/what-is-kanban/' },
-            { name: 'Lean', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Lean_Enterprise_Institute_logo.svg/1200px-Lean_Enterprise_Institute_logo.svg.png', url: 'https://www.lean.org/' }
+            { name: 'Scrum', image: 'https://cdn.simpleicons.org/scrumalliance/009FDA', url: 'https://www.scrum.org/' },
+            { name: 'Kanban', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Kanban_board_example.jpg/300px-Kanban_board_example.jpg', url: 'https://kanbanize.com/kanban-resources/getting-started/what-is-kanban/' },
         ]
     },
     version_controllers: {
@@ -84,6 +83,7 @@ export const siteLinks = {
         { href: "/", label: "Início", icon: "fa-house" },
         { href: "/downloads", label: "Downloads", icon: "fa-download", newTab: false },
         { href: "/experience", label: "Experiência", icon: "fa-briefcase" },
+        { href: "/portfolio", label: "Portfólio", icon: "fa-folder-open" },
         { href: "/social", label: "Social", icon: "fa-hashtag" },
     ],
     external: [
