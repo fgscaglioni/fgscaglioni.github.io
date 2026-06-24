38c1f48e9b24ecd5168e65c67a0d72dd5c75d74a
Fabrício Scaglioni <fgscaglioni@gmail.com>
2026-06-24 11:58:51 -0300

    feat(site): redesign tema Papel, componentes reutilizáveis, stack enxuta
    - Redesign completo: dark glassmorphism -> tema Papel (off-white + teal)
    - Componentes: PaperCard, PageHeader, SectionHeader, BulletItem, Footer
    - Navbar adaptada ao tema claro com navegação teal
    - Stack reduzida de 33 para 8 tecnologias essenciais
    - StackList em grid plano sem subdivisões
    - Lockfile migrado para pnpm (consistente com CI)
    - site: fgscaglioni.github.io -> fgscaglioni.dev.br
    - Open Graph, Twitter Cards, focus-visible, favicon customizado
    - Blog prose adaptado ao tema claro
    - Contraste de subtítulos corrigido (gray-400 -> gray-500)
    - Links externos funcionando dentro de cards
    - 23 resquícios do tema dark eliminados

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fgscaglioni.github.io',
+  site: 'https://fgscaglioni.dev.br',
   vite: {
     plugins: [tailwindcss()]
   }
--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -1,77 +1,13 @@
-export const my_stack = {
-    languages: {
-        color: '#2dd4bf', // Teal moderno e sofisticado
-        title: 'Linguagens',
-        data: [
-            { name: 'Java', image: 'https://blog.geekhunter.com.br/wp-content/uploads/2020/07/pngwing.com_.png', url: 'https://www.java.com/' },
-            { name: 'PHP', image: 'https://e7.pngegg.com/pngimages/185/655/png-clipart-logo-php-computer-icons-symbol-miscellaneous-emblem-thumbnail.png', url: 'https://www.php.net/' },
-            { name: 'JavaScript', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuHnJDLOcdm_0b6N6kNj-1OvO9KhKYgqIy0w&s', url: '' },
-            { name: 'TypeScript', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Typescript.svg/1200px-Typescript.svg.png', url: 'https://www.typescriptlang.org/' },
-            { name: 'Python', image: 'https://raw.githubusercontent.com/docker-library/docs/01c12653951b2fe592c1f93a13b4e289ada0e3a1/python/logo.png', url: 'https://www.python.org/' },
-            { name: 'HTML', image: 'https://bognarjunior.wordpress.com/wp-content/uploads/2014/12/1417589451_html-256.png', url: '' },
-            { name: 'CSS', image: 'https://ralfvanveen.com/wp-content/uploads/2021/06/CSS-_-Begrippenlijst.svg', url: '' },
-        ]
-    },
-    databases: {
-        color: '#fb7185', // Rosa suave equilibrado
-        title: 'Bancos de Dados',
-        data: [
-            { name: 'PostgreSQL', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/640px-Postgresql_elephant.svg.png', url: 'https://www.postgresql.org/' },
-            { name: 'MySQL', image: 'https://www.ovhcloud.com/sites/default/files/styles/large_screens_1x/public/2021-09/ECX-1909_Hero_MySQL_600x400%402x-1_0.png', url: 'https://www.mysql.com/' },
-            { name: 'SQLite', image: 'https://www.sqlite.org/images/sqlite370_banner.gif', url: 'https://www.sqlite.org/' },
-            { name: 'MongoDB', image: 'https://osbsoftware.com.br/upload/produto/mongodb.png', url: 'https://www.mongodb.com/' },
-            { name: 'Redis', image: 'https://blog.patrickbrandao.com/wp-content/uploads/2024/11/redis-logo-512.png', url: 'https://redis.io/' },
-            { name: 'Firestore', image: 'https://emendes.com/wp-content/uploads/2019/11/Cloud-Firestore-3-Vertical-Lockup-Light.png', url: 'https://firebase.google.com/docs/firestore' },
-            { name: 'Oracle', image: 'https://www.nrb.be/sites/default/files/oracle.png', url: 'https://www.oracle.com/database/' },
-        ]
-    },
-    frameworks: {
-        color: '#818cf8', // Indigo vibrante e legível
-        title: 'Frameworks & Bibliotecas',
-        data: [
-            { name: 'Laravel', image: 'https://laravel.com/img/logotype.min.svg', url: 'https://laravel.com/' },
-            { name: 'CodeIgniter', image: 'https://packagecontrol.io/readmes/img/dfb5fec47c7a7e44d09fb1631dfe0f41806bce27.png', url: 'https://www.codeigniter.com/' },
-            { name: 'Ionic', image: 'https://www.wonderpush.com/wp-content/uploads/2019/09/ionic.png', url: 'https://ionicframework.com/' },
-            { name: 'Angular', image: 'https://angular.io/assets/images/logos/angular/angular.svg', url: 'https://angular.dev/' },
-            { name: 'NestJS', image: 'https://nestjs.com/img/logo-small.svg', url: 'https://nestjs.com/' },
-            { name: 'React', image: 'https://images.seeklogo.com/logo-png/27/2/react-logo-png_seeklogo-273845.png', url: 'https://react.dev/' },
-            { name: 'Svelte', image: 'https://svelte.dev/svelte-logo-horizontal.svg', url: 'https://svelte.dev/' },
-            { name: 'Apache Cordova', image: 'https://cordova.apache.org/static/img/cordova_bot.png', url: 'https://cordova.apache.org/' },
-            { name: 'jQuery', image: 'https://miro.medium.com/v2/resize:fit:860/0*eFomJUFua8tuqe8g.png', url: 'https://jquery.com/' },
-        ]
-    },
-    others: {
-        color: '#f97316', // Laranja moderno (Orange 500)
-        title: 'Outras Tecnologias',
-        data: [
-            { name: 'Android', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDHvnyoyWI-FlgTfOWRoJjazD3ke1Q6hOgfQ&s', url: 'https://www.android.com/intl/pt_br/' },
-            { name: 'n8n', image: 'https://registry.npmmirror.com/@lobehub/icons-static-png/latest/files/dark/n8n-color.png', url: 'https://n8n.io/' },
-            { name: 'Coolify', image: 'https://docs.hetzner.com/static/1dbc8e5220638f7193ef9f5a24c2eb5b/0b533/coolify-logo.png', url: 'https://coolify.io/' },
-            { name: 'Easypanel', image: 'https://avatars.githubusercontent.com/u/97469292?s=200&v=4', url: 'https://easypanel.io/' },
-            { name: 'Firebase', image: 'https://firebase.google.com/static/images/brand-guidelines/logo-vertical.png?hl=pt-br', url: 'https://firebase.google.com/' },
-            { name: 'Docker', image: 'https://raw.githubusercontent.com/collabnix/dockerlabs/master/beginners/docker/images/docker_facebook_share.png', url: 'https://www.docker.com/' },
-            { name: 'Docsify', image: 'https://images.opencollective.com/docsify/5916949/logo/256.png', url: 'https://docsify.js.org/' },
-            { name: 'Unsloth', image: 'https://avatars.githubusercontent.com/u/150920049?s=280&v=4', url: 'https://unsloth.ai/' }
-        ]
-    },
-    methodologies: {
-        color: '#64748b', // Slate discreto
-        title: 'Metodologias',
-        data: [
-            { name: 'Scrum', image: 'https://cdn.simpleicons.org/scrumalliance/009FDA', url: 'https://www.scrum.org/' },
-            { name: 'Kanban', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Kanban_board_example.jpg/300px-Kanban_board_example.jpg', url: 'https://kanbanize.com/kanban-resources/getting-started/what-is-kanban/' },
-        ]
-    },
-    version_controllers: {
-        color: '#4ade80', // Verde menta vivo (Green 400)
-        title: 'Controle de Versão',
-        data: [
-            { name: 'Github', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/250px-GitHub_Invertocat_Logo.svg.png', url: 'https://github.com/' },
-            { name: 'Gitlab', image: 'https://yt3.googleusercontent.com/Zfx2OSI5XWKhnove-HwdTokQUmCB7cfpX0uGbqxMeQVNMzVPrvgMadFHpbfYoy04NrjFEBk_3A=s900-c-k-c0x00ffffff-no-rj', url: 'https://about.gitlab.com/' },
-            { name: 'Bitbucket', image: 'https://images.prismic.io/guardrails-01/c7345dce-8605-4c3e-81e8-30a711d17e89_bitbucket.png?auto=compress,format', url: 'https://bitbucket.org/product/' },
-        ]
-    }
-};
+export const my_stack = [
+    { name: 'Python', image: 'https://raw.githubusercontent.com/docker-library/docs/01c12653951b2fe592c1f93a13b4e289ada0e3a1/python/logo.png', url: 'https://www.python.org/' },
+    { name: 'PHP', image: 'https://e7.pngegg.com/pngimages/185/655/png-clipart-logo-php-computer-icons-symbol-miscellaneous-emblem-thumbnail.png', url: 'https://www.php.net/' },
+    { name: 'TypeScript', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg', url: 'https://www.typescriptlang.org/' },
+    { name: 'PostgreSQL', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg', url: 'https://www.postgresql.org/' },
+    { name: 'Laravel', image: 'https://laravel.com/img/logotype.min.svg', url: 'https://laravel.com/' },
+    { name: 'NestJS', image: 'https://nestjs.com/img/logo-small.svg', url: 'https://nestjs.com/' },
+    { name: 'Svelte', image: 'https://svelte.dev/svelte-logo-horizontal.svg', url: 'https://svelte.dev/' },
+    { name: 'Docker', image: 'https://raw.githubusercontent.com/collabnix/dockerlabs/master/beginners/docker/images/docker_facebook_share.png', url: 'https://www.docker.com/' },
+];
 
 export const siteLinks = {
     main: [
